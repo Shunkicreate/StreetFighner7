864f221fe19389f7aa5473cbc76925e6fa967e8e
kenta-yamada <kenta.yamada@dena.com>
2024-09-08 02:46:32 +0900

    リザルト画面の修正

--- StreetFighner7/Views/ResultView.swift
+++ StreetFighner7/Views/ResultView.swift
@@ -9,92 +9,70 @@ struct ResultView: View {
     let colorOpacity: Double = 0.6
 
     var body: some View {
-        ZStack {
-            // 背景画像を追加
-            Image("background") // 背景画像名を指定
-                .resizable() // 画像をリサイズ可能に
-                .scaledToFill() // 画像をフレーム全体にフィット
-                .ignoresSafeArea() // セーフエリアを無視して全画面に表示
+        GeometryReader { geometry in
+            ZStack {
+                // リザルト画面のコンテンツ
+                VStack {
+                    Text("リザルト")
+                        .font(Font.custom("Mimi_font-Regular", size: 40))
+                        .padding()
+                    
+                    HStack {
+                        // 猫画像
+                        Image("neko_ilust_1").resizable() // Make the image resizable
+                            .scaledToFit() // Scale it proportionally
+                            .frame(width: geometry.size.width * 0.4)
+                            .padding()
+                        // 猫の結果
+                        VStack(alignment: .leading) {
+                            Text("猫")
+                                .font(Font.custom("Mimi_font-Regular", size: 35))
+                                .padding(.bottom, 5)
+                            
+                            Text("叩いた回数")
+                            Text("\(resultScore.totalSuccess+resultScore.totalFailure)回")
+                                .padding(.bottom, 5)
+                            
+                            Text("取得したチュール")
+                            Text("\(resultScore.totalSuccess)個")
+                            
+                            Text("成功率")
+                            Text("\(Int(Double(resultScore.totalSuccess) / Double(resultScore.totalSuccess + resultScore.totalFailure) * 100))%")
 
-            // リザルト画面のコンテンツ
-            VStack {
-                Text("リザルト")
-                    .font(Font.custom("Mimi_font-Regular", size: 40))
-                    .padding()
-
-                HStack {
-                    // 猫の結果
-                    VStack(alignment: .leading) {
-                        Text("猫")
-                            .font(Font.custom("Mimi_font-Regular", size: 35))
-                            .padding(.bottom, 5)
-
-                        Text("叩いた回数")
-                        Text("\(resultScore.totalSuccess+resultScore.totalFailure)回")
-                            .padding(.bottom, 5)
-
-                        Text("取得したチュール")
-                        Text("\(resultScore.totalSuccess)個")
+                        }
+                        .padding()
+                        .background(Color.gray.opacity(colorOpacity)) // 背景色
+                        .cornerRadius(10)
+                        .frame(maxWidth: .infinity) // 横幅を自動調整
                     }
-                    .padding()
-                    .background(Color.gray.opacity(colorOpacity)) // 背景色
-                    .cornerRadius(10)
-                    .frame(maxWidth: .infinity) // 横幅を自動調整
-
-                    // 飼い主の結果
-                    VStack(alignment: .leading) {
-                        Text("かいぬし")
-                            .font(Font.custom("Mimi_font-Regular", size: 35))
-                            .padding(.bottom, 5)
-
-                        Text("避けた回数")
-                        Text("\(resultScore.totalAvoid)回")
-                            .padding(.bottom, 5)
-
-                        Text("あげてしまったチュール")
-                        Text("\(resultScore.totalFailure)個")
-                    }
-                    .padding()
-                    .background(Color.gray.opacity(colorOpacity)) // 背景色
-                    .cornerRadius(10)
-                    .frame(maxWidth: .infinity) // 横幅を自動調整
+                    
+                    Spacer()
+                    // タイトル画面に戻るボタン
+                    NavigationLink("トップへ戻る", destination: TitleView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult))
+                        .onTapGesture {
+                            isFromResult = true // 遷移元がResultであることを設定
+                            resultScore.resetScores() // スコアをリセット
+                        }
+                        .padding()
+                        .background(Color.gray.opacity(colorOpacity))
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                    
+                    
                 }
                 .padding()
-
+                
                 Spacer()
-
-                // タイトル画面に戻るボタン
-                NavigationLink("トップへ戻る", destination: TitleView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult))
-                    .onTapGesture {
-                        isFromResult = true // 遷移元がResultであることを設定
-                        resultScore.resetScores() // スコアをリセット
-                    }
-                    .padding()
-                    .background(Color.gray.opacity(colorOpacity))
-                    .foregroundColor(.black)
-                    .cornerRadius(10)
             }
+            .navigationBarBackButtonHidden(true)
             .padding()
-
-            Spacer()
-
-            // タイトル画面に戻るボタン
-            NavigationLink("Back to Title", destination: TitleView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult))
-                .onTapGesture {
-                    isFromResult = true // 遷移元がResultであることを設定
-                }
-                .padding()
-                .background(Color.gray.opacity(colorOpacity))
-                .foregroundColor(.black)
-                .cornerRadius(10)
-        }
-        .navigationBarBackButtonHidden(true)
-        .padding()
-        .background {
-            Image("background")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
+            .background {
+                Image("background")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            }
+            
         }
     }
 }
