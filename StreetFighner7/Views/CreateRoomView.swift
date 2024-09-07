4f0d7edcb0e3c2459410aafcc2ccb2709e9cbcf9
shunsuke-tamura <wwwxx4869@gmail.com>
2024-09-08 05:18:02 +0900

    imple p2p on create room

--- StreetFighner7/Views/CreateRoomView.swift
+++ StreetFighner7/Views/CreateRoomView.swift
@@ -10,14 +10,9 @@ struct CreateRoomView: View {
     @Binding var path: NavigationPath
     @Binding var isFromResult: Bool
     @Environment(\.dismiss) var dismiss
-    
-    @State private var users: [User] = [
-            User(name: "User1"),
-            User(name: "User2"),
-            User(name: "User3")
-        ]
-    // 選択されたユーザー
-        @State private var selectedUser: User? = nil
+    @ObservedObject var viewModel = CreateRoomViewModel()
+    @State var isPleaseWaitAlertActive: Bool = false
+    @State private var isNavigationActive = false
     
     var body: some View {
         NavigationView {
@@ -26,27 +21,27 @@ struct CreateRoomView: View {
                     .font(Font.custom("Mimi_font-Regular", size: 96))
                 ScrollView {
                     VStack(spacing: 15) {
-                        ForEach(users) { user in
+                        ForEach(viewModel.peers) { peer in
                             Button(action: {
-                                selectedUser = user
+                                viewModel.invite(_selectedPeer: peer)
                             }) {
                                 HStack {
-                                    Text(user.name)
+                                    Text(peer.peerId.displayName)
                                         .font(Font.custom("Mimi_font-Regular", size: 18)) // Reduce the font size
                                         .padding(.vertical, 10) // Adjust vertical padding to make buttons smaller
                                         .padding(.horizontal, 15) // Adjust horizontal padding
-                                        .foregroundColor(selectedUser?.id == user.id ? .white : .black)
+                                        .foregroundColor(viewModel.selectedPeer?.peerId == peer.peerId ? .white : .black)
                                     
                                     Spacer()
                                     
-                                    if selectedUser?.id == user.id {
+                                    if viewModel.selectedPeer?.peerId == peer.peerId {
                                         Image(systemName: "checkmark")
                                             .foregroundColor(.white)
                                             .padding(.trailing, 10)
                                     }
                                 }
                                 .frame(maxWidth: UIScreen.main.bounds.width * 0.8) // Set width to 80% of the screen
-                                .background(selectedUser?.id == user.id ? Color.blue : Color.white)
+                                .background(viewModel.selectedPeer?.peerId == peer.peerId ? Color.blue : Color.white)
                                 .cornerRadius(15)
                                 .shadow(color: .gray.opacity(0.3), radius: 5, x: 0, y: 5)
                                 .padding(.horizontal, 10) // Adjust padding to center the buttons
@@ -54,18 +49,36 @@ struct CreateRoomView: View {
                         }
                     }
                 }
-                NavigationLink(destination: AttackView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult)) {
+                Button(action:{
+                    // 条件をチェックして、遷移を許可するかを決定
+                    if (!viewModel.isParticipantsJoined()) {
+                        isPleaseWaitAlertActive = true
+                        return
+                    }
+                    viewModel.join()
+                    isNavigationActive = true
+                }) {
                     Text("たたかう")
                         .font(Font.custom("Mimi_font-Regular", size: 24))
                         .padding()
                         .frame(width: 250, height: 65)
-                        .background(selectedUser == nil ? Color.gray : Color.black) // Change background to gray if disabled
-                        .foregroundColor(selectedUser == nil ? Color.black : Color.white) // Change text color to black if disabled
+                        .background(viewModel.selectedPeer == nil ? Color.gray : Color.black) // Change background to gray if disabled
+                        .foregroundColor(viewModel.selectedPeer == nil ? Color.black : Color.white) // Change text color to black if disabled
                         .cornerRadius(.infinity)
                 }
-                .disabled(selectedUser == nil) // Disable button if no user is selected
+                .disabled(viewModel.selectedPeer == nil) // Disable button if no user is selected
                 .padding(5)
-
+                .alert(isPresented: $isPleaseWaitAlertActive, content: {
+                    Alert(
+                        title: Text("まだその時ではないニャ"),
+                        dismissButton: .default(Text("OK"), action: {
+                            isPleaseWaitAlertActive = false
+                        })
+                    )
+                })
+                NavigationLink(destination: AttackView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult), isActive: $isNavigationActive) {
+                    EmptyView()
+                }
             }
             .padding()
             .background {
