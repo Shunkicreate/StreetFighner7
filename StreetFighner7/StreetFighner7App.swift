a167638542999adc118c2b2d301b62f3be94ec72
shunki-tada <shunki.tada@dena.jp>
2024-09-07 15:44:44 +0900

    Initial Commit

---
+++ StreetFighner7/StreetFighner7App.swift
@@ -0,0 +1,17 @@
+//
+//  StreetFighner7App.swift
+//  StreetFighner7
+//
+//  Created by Shunki Tada on 2024/09/07.
+//
+
+import SwiftUI
+
+@main
+struct StreetFighner7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
