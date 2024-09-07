debc04fb03807d5aacb594d7da9cd1dc1e4b2ef6
taishin-miyamoto <taishin.miyamoto@dena.com>
2024-09-07 22:46:00 +0900

    背景画像をbackgroundで指定

--- StreetFighner7/Views/TitleView.swift
+++ StreetFighner7/Views/TitleView.swift
@@ -6,42 +6,40 @@ struct TitleView: View {
     @Binding var isFromResult: Bool
     
     var body: some View {
-        ZStack {
-            // 背景画像を追加
-            Image("background")
-                .resizable() // 画像をリサイズ可能にする
-                .scaledToFill() // 画像をフレーム全体にフィットさせる
-                .ignoresSafeArea() // セーフエリアを無視して全画面に表示する
+        VStack {
+            Text("ねこぱんち")
+                .font(Font.custom("Mimi_font-Regular", size: 96))
             
-            // 他のUIコンポーネントを配置
-            VStack {
-                Text("ねこぱんち")
-                    .font(Font.custom("Mimi_font-Regular", size: 96))
-                
-                NavigationLink(destination: CreateRoomView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult)) {
-                    Text("へやをつくる")
-                        .font(Font.custom("Mimi_font-Regular", size: 24))
-                        .padding()
-                        .accentColor(Color.white)
-                        .frame(width: 250, height: 65)
-                        .background(Color.black)
-                        .cornerRadius(.infinity)
-                }
-                .padding(10)
-                
-                NavigationLink(destination: JoinRoomView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult)) {
-                    Text("へやにはいる")
-                        .font(Font.custom("Mimi_font-Regular", size: 24))
-                        .padding()
-                        .accentColor(Color.white)
-                        .frame(width: 250, height: 65)
-                        .background(Color.black)
-                        .cornerRadius(.infinity)
-                }
-                .padding(5)
+            NavigationLink(destination: CreateRoomView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult)) {
+                Text("へやをつくる")
+                    .font(Font.custom("Mimi_font-Regular", size: 24))
+                    .padding()
+                    .accentColor(Color.white)
+                    .frame(width: 250, height: 65)
+                    .background(Color.black)
+                    .cornerRadius(.infinity)
             }
-            .padding()
-            .navigationBarBackButtonHidden(true)
+            .padding(10)
+            
+            NavigationLink(destination: JoinRoomView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult)) {
+                Text("へやにはいる")
+                    .font(Font.custom("Mimi_font-Regular", size: 24))
+                    .padding()
+                    .accentColor(Color.white)
+                    .frame(width: 250, height: 65)
+                    .background(Color.black)
+                    .cornerRadius(.infinity)
+            }
+            .padding(5)
+        }
+        .padding()
+        .navigationBarBackButtonHidden(true)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
         }
     }
 }
