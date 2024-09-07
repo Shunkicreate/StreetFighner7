9e93ffd476aeb8c2c6599d1b5611223709bb54b8
toma.tanigawa <toma.tanigawa@dena.jp>
2024-09-08 04:37:00 +0900

    attackの実装

--- StreetFighner7/Managers/MotionManager.swift
+++ StreetFighner7/Managers/MotionManager.swift
@@ -36,17 +36,18 @@ final class MotionManager: ObservableObject {
             }
             self.accelerometerData = data
             
-            print("x: \(self.accelerometerData?.acceleration.x), y: \(self.accelerometerData?.acceleration.y), z: \(self.accelerometerData?.acceleration.z)")
+//            print("x: \(self.accelerometerData?.acceleration.x), y: \(self.accelerometerData?.acceleration.y), z: \(self.accelerometerData?.acceleration.z)")
             
             // MARK: 猫パンチ判定
             // 調整可能な閾値。デバイスを振ったと見なす加速度の値。
             let yThreshold: Double = 1.5
-            let zThreshold: Double = 3.5
+            let zThreshold: Double = 1.2
 
-            if 
-                (fabs(data.acceleration.y) > yThreshold || fabs(data.acceleration.z) > zThreshold),
+            if
+//                (fabs(data.acceleration.y) > yThreshold || fabs(data.acceleration.z) > zThreshold),
+                (fabs(data.acceleration.z) > zThreshold),
                 let previousZ = self.previousZ,
-                previousZ > 0 {
+                previousZ > 0 && data.acceleration.z < 0 {
                 self.isAttack = true
 //                self.direction = CatHandDirection.calcDirection(roll: data.acceleration.x)
 //                print(self.direction?.rawValue)
--- StreetFighner7/Views/AttackView.swift
+++ StreetFighner7/Views/AttackView.swift
@@ -6,29 +6,64 @@ struct AttackView: View {
     @Binding var isFromResult: Bool
     @StateObject private var resultScore = ResultScore()
     @StateObject private var motionManager = MotionManager()
-    @State private var CatHandModel = (isAttack: false, position: CatHandDirection.center)
+    @State private var catHandModel = CatHandModel(position: CatHandDirection.center)
     
     var body: some View {
-        VStack {
-            Text("Attack Screen")
-                .font(.largeTitle)
-            NavigationLink(destination: ResultView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult, resultScore: resultScore)) {
-                Text("り ざ る と")
-                    .font(Font.custom("Mimi_font-Regular", size: 24))
-                    .padding()
-                    .accentColor(Color.white)
-                    .frame(width: 250, height: 65)
-                    .background(Color.black)
-                    .cornerRadius(.infinity)
+        GeometryReader { geometry in
+            ZStack {
+                if 1 == 1 {
+                    if catHandModel.direction == .left {
+                        Image("nekonote_reverse")
+                            .resizable()
+                            .scaledToFit()
+                            .position(x: geometry.size.width * 0.2, y: geometry.size.height * 0.4)
+                    } else if catHandModel.direction == .center {
+                        Image("nekonote_reverse")
+                            .resizable()
+                            .scaledToFit()
+                            .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.4)
+                    } else if catHandModel.direction == .right {
+                        Image("nekonote_reverse")
+                            .resizable()
+                            .scaledToFit()
+                            .position(x: geometry.size.width * 0.8, y: geometry.size.height * 0.4)
+                    }
+                    // アタック時のオーバーレイ
+                    ZStack {
+                        Image("concentration_line")
+                            .resizable()
+                            .scaledToFill()
+                            .opacity(0.5) // 半透明にして重ね合わせる
+                            .ignoresSafeArea()
+                    }
+                } else {
+                    VStack {
+                        Text("Attack Screen")
+                            .font(.largeTitle)
+                        NavigationLink(destination: ResultView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult, resultScore: resultScore)) {
+                            Text("り ざ る と")
+                                .font(Font.custom("Mimi_font-Regular", size: 24))
+                                .padding()
+                                .accentColor(Color.white)
+                                .frame(width: 250, height: 65)
+                                .background(Color.black)
+                                .cornerRadius(.infinity)
+                        }
+                    }
+                }
             }
         }
-        .padding()
-        .navigationBarBackButtonHidden(true)
+//        .padding()
+//        .navigationBarBackButtonHidden(true)
         .onAppear {
             rotateScreenModel.rotateScreen(orientation: .portrait)
+            motionManager.startAccelerometer(interval: 0.1)
+            motionManager.startDeviceMotion(interval: 0.1)
         }
         .onDisappear {
             rotateScreenModel.rotateScreen(orientation: .landscapeLeft)
+            motionManager.stopAccelerometer()
+            motionManager.stopDeviceMotion()
         }
         .onChange(of: motionManager.accelerometerData) { _ in
             updateCatHandModel() // 加速度データが変わるたびに状態を更新
@@ -37,8 +72,33 @@ struct AttackView: View {
     
     private func updateCatHandModel() {
         var isAttack = motionManager.isAttack;
+        var accelerometerData = motionManager.accelerometerData
         var deviceMotionData = motionManager.deviceMotionData
         
+        self.catHandModel.updateIsAttack(isAttack: (isAttack ?? false))
+            
+        if isAttack == false {
+            return
+        }
+       //
+       var yaw = (deviceMotionData?.attitude.yaw ?? 90) * 180 / Double.pi
+       let _ = print(yaw)
+       yaw -= 20
+            
+       //    if yaw <= -90 {
+       //      yaw += 180
+       //    }
+       //    if yaw >= 90 {
+       //      yaw -= 180
+       //    }
+            
+       if(yaw >= 30 && yaw <= 150) {
+         self.catHandModel.updateDirection(direction: CatHandDirection.left)
+       } else if (yaw <= -30 && yaw >= -150) {
+         self.catHandModel.updateDirection(direction: CatHandDirection.right)
+       } else {
+         self.catHandModel.updateDirection(direction: CatHandDirection.center)
+       }
     }
 }
 
