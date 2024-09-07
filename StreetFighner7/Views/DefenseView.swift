39102e477263180b2f646a8f82864f31f84342cb
kenta-yamada <kenta.yamada@dena.com>
2024-09-07 18:19:40 +0900

    HITを作成

--- StreetFighner7/Views/DefenseView.swift
+++ StreetFighner7/Views/DefenseView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct DefenseView: View {
     @State private var gameModel = NekonoteModel(state: .center)
+    @State private var isAttacked = false // 新しい状態
     @Binding var path: NavigationPath
     @Binding var isFromResult: Bool
     
@@ -13,6 +14,7 @@ struct DefenseView: View {
                     .scaledToFill()
                     .ignoresSafeArea()
                 
+                // Nekonote状態に応じた画像の表示
                 ZStack {
                     if gameModel.state == .left {
                         Image("nekonote_reverse")
@@ -30,17 +32,29 @@ struct DefenseView: View {
                             .scaledToFit()
                             .position(x: geometry.size.width * 0.8, y: geometry.size.height * 0.3)
                     }
+                    
+                    // アタック時のオーバーレイ
+                    if isAttacked {
+                        Image("concentration_line")
+                            .resizable()
+                            .scaledToFill()
+                            .opacity(0.5) // 半透明にして重ね合わせる
+                            .ignoresSafeArea()
+                    }
                 }
                 
                 VStack {
                     HStack {
                         Button("Left") {
+                            handleAttack()
                             gameModel.state = .left
                         }
                         Button("Center") {
+                            handleAttack()
                             gameModel.state = .center
                         }
                         Button("Right") {
+                            handleAttack()
                             gameModel.state = .right
                         }
                         NavigationLink("Go to Result", destination: ResultView(path: $path, isFromResult: $isFromResult))
@@ -50,6 +64,14 @@ struct DefenseView: View {
                 .padding()
                 .navigationBarBackButtonHidden(true)
             }
+        }
+    }
+    
+    private func handleAttack() {
+        // アタックされたときの処理
+        isAttacked = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            isAttacked = false
         }
     }
 }
