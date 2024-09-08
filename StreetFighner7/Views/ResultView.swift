7e5809a75311db913ea04c413b457aeb2527653c
taishin-miyamoto <taishin.miyamoto@dena.com>
2024-09-08 10:04:41 +0900

    リザルト画面のデザインを修正

--- StreetFighner7/Views/ResultView.swift
+++ StreetFighner7/Views/ResultView.swift
@@ -6,7 +6,7 @@ struct ResultView: View {
     @Binding var isFromResult: Bool
     @ObservedObject var resultScore: ResultScore // ResultScoreを追加
 
-    let colorOpacity: Double = 0.6
+    let colorOpacity: Double = 1.0
     // 結果のねこの太り具合を調整
     let neko_fat_standard_score = 10
     let neko_fat_standard_level = 5
@@ -20,63 +20,60 @@ struct ResultView: View {
             return "neko_ilust_fat_" + String(neko_fat_level)
         }
         GeometryReader { geometry in
-            ZStack {
-                // リザルト画面のコンテンツ
-                VStack {
-                    Text("リザルト")
-                        .font(Font.custom("Mimi_font-Regular", size: 40))
-                        .padding()
-                    
-                    HStack {
+            VStack {
+                Text("リザルト")
+                    .font(Font.custom("Mimi_font-Regular", size: 40))
+                    .padding()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .trailing) {
                         // 猫画像
                         Image(neko_fat_image).resizable() // Make the image resizable
                             .scaledToFit() // Scale it proportionally
                             .frame(height: geometry.size.height * 0.5)
+                        
+                        // タイトル画面に戻るボタン
+                        NavigationLink("トップへ戻る", destination: TitleView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult))
+                            .onTapGesture {
+                                isFromResult = true // 遷移元がResultであることを設定
+                                resultScore.resetScores() // スコアをリセット
+                            }
                             .padding()
-                        // 猫の結果
-                        VStack(alignment: .leading) {
-                            Text("猫")
-                                .font(Font.custom("Mimi_font-Regular", size: 35))
-                                .padding(.bottom, 5)
-                            
-                            Text("叩いた回数")
-                            Text("\(resultScore.totalSuccess+resultScore.totalFailure)回")
-                                .padding(.bottom, 5)
-                            
-                            Text("取得したチュール")
-                            Text("\(resultScore.totalSuccess)個")
-                            
-                            Text("成功率")
-                            let totalAttempts = resultScore.totalSuccess + resultScore.totalFailure
-                            let successPercentage = totalAttempts > 0 ? Int(Double(resultScore.totalSuccess) / Double(totalAttempts) * 100) : 0
-
-                            Text("\(successPercentage)%")
-
-                        }
-                        .padding()
-                        .background(Color.gray.opacity(colorOpacity)) // 背景色
-                        .cornerRadius(10)
-                        .frame(maxWidth: .infinity) // 横幅を自動調整
+                            .background(Color.black)
+                            .foregroundStyle(.white)
+                            .cornerRadius(10)
+                            .font(Font.custom("Mimi_font-Regular", size: 20))
                     }
-                    
-                    Spacer()
-                    // タイトル画面に戻るボタン
-                    NavigationLink("トップへ戻る", destination: TitleView(rotateScreenModel: rotateScreenModel, path: $path, isFromResult: $isFromResult))
-                        .onTapGesture {
-                            isFromResult = true // 遷移元がResultであることを設定
-                            resultScore.resetScores() // スコアをリセット
-                        }
-                        .padding()
-                        .background(Color.gray.opacity(colorOpacity))
-                        .foregroundColor(.black)
-                        .cornerRadius(10)
-                    
-                    
+
+                    // 猫の結果
+                    VStack(alignment: .leading) {
+                        Text("猫")
+                            .padding(.bottom, 5)
+                        
+                        Text("叩いた回数")
+                        Text("\(resultScore.totalSuccess+resultScore.totalFailure)回")
+                            .padding(.bottom, 5)
+                        
+                        Text("取得したチュール")
+                        Text("\(resultScore.totalSuccess)個")
+                        
+                        Text("成功率")
+                        let totalAttempts = resultScore.totalSuccess + resultScore.totalFailure
+                        let successPercentage = totalAttempts > 0 ? Int(Double(resultScore.totalSuccess) / Double(totalAttempts) * 100) : 0
+
+                        Text("\(successPercentage)%")
+
+                    }
+                    .font(Font.custom("Mimi_font-Regular", size: 25))
+                    .padding()
+                    .frame(width: 500, alignment: .leading)
+                    .background(Color.gray.opacity(0.8))
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
                 }
-                .padding()
-                
-                Spacer()
+                .offset(y: -10)
             }
+            .padding()
             .navigationBarBackButtonHidden(true)
             .padding()
             .background {
